c12dc308c8a41307bd264166d3c085687a8e5b18
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2021-07-04 10:18:05 +0000

    Update pricetest.html

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,13 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   # GET /tweets
-  # GET /tweets.js
+  # GET /tweets.json
+  def index
+    @tweets = Tweet.all
+  end
+
+  # GET /tweets/1
+  # GET /tweets/1.json
+  def show
+  end
+
