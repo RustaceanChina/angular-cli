9b05645c6e35df2040d430d014f64713ef6706e9
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2021-07-07 15:23:31 +0000

    :neckbeard: Added .gitattributes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,4 +29,7 @@ class TweetsController < ApplicationController
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :sh
+        format.json { render :show, status: :created, location: @tweet }
+      else
+        format.html { render :new }
+        format.json { render 
