433bf75d5143846460de5a909920bc9d0ee161f6
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2021-07-22 07:22:08 +0000

    WL6559    Optimize GTIDs for passive slave - store GTIDs in table

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -64,4 +64,7 @@ class TweetsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tweet
-      @tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only all
