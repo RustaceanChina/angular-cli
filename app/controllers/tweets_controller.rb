7e098599f8ae7107ec150f961284479ac81691df
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2021-07-22 10:15:15 +0000

    Updating packages.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -67,4 +67,8 @@ class TweetsController < ApplicationController
       @tweet = Tweet.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only all
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tweet_params
+      params.fetch(:tweet, {})
+    end
+end
