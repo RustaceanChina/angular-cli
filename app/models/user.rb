6eee36cbc85b5f456f7cd3be05f407e8a19413ad
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2021-08-07 20:31:43 +0000

    debug removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   def self.find_or_create_from_auth_hash(auth_hash)
-    user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_o
+    user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
+    user.update(
+      name: auth_hash.info.nickname,
+      token: auth_hash.credentials.token,
+      secret: aut
