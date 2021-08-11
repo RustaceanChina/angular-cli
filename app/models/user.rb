46ee1cbf994368cc3b4f5b06c6294d08a17c4349
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2021-08-11 20:26:00 +0000

    Example tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
   def twitter
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = Settings.twitter_api_key
-      con
+      config.consumer_secret     = Settings.twitter_api_secret
+      config.access_token        = token
+      config.access_token_secr
