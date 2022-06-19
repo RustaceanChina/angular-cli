d7e2d9cdfc135169c7ad6fd27a2c7a6b5541b47f
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-06-19 07:28:21 +0000

    Remove obsolete import

--- config/application.rb
+++ config/application.rb
@@ -2,4 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, inc
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+mod
