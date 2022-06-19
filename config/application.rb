314d7fec4da38970dc7b0dc805028b8313cc2b52
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-06-19 21:17:45 +0000

    #89

--- config/application.rb
+++ config/application.rb
@@ -6,4 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-mod
+module Categorizer
+  class Application < Rails::Application
+    # Settings in config/environments/* take prece
