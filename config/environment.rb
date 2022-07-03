f11b578b23dff86dad20cf0c834cae54cb85ebe1
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-03 09:49:56 +0000

    release 0.0.3

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.app
+Rails.application.initialize!
---
+++ config/environments/development.rb
@@ -0,0 +1,2 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in
