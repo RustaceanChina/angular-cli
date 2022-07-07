ac928d596d1e57dc05e1eeaed7bc1fb75f404deb
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-07 23:00:59 +0000

    change settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,7 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public,
