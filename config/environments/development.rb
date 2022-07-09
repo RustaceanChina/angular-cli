31a420656d8ae9773c1162edea039a906d286a23
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-09 15:39:16 +0000

    table initial

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,4 +18,9 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public,
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = 
