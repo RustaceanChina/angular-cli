93f7639379a138afe03465cc9cab81cea0a7f84e
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-10 14:19:56 +0000

    Fix 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,8 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = 
+    config.cache_store = :null_store
+  end
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_de
