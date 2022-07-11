df828f13814ff67d831c3a1f455ef4d7dcebc2f5
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-11 06:09:02 +0000

    Syntax Fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,8 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_de
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  # Print deprecation notices to the Rails l
