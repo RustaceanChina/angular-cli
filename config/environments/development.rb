d17e5fd7d9d99b40d9f33f905ca9c7b974478b45
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-18 05:46:28 +0000

    Update withS3Instance.groovy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,9 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.a
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+ 
