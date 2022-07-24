d1c03b0d7ee41bcad6527e428fd82f8116ec932d
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-24 08:08:21 +0000

    Fix bad test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,4 +7,6 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Do not eager load code on boot. This avoi
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # pr
