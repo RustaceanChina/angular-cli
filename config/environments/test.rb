5d54154488a4d50e2f5eaee2465ec100da22a6f0
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-07-30 13:28:04 +0000

    dns_resolver: Null-terminate the right string

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,4 +25,6 @@ Rails.application.configure do
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery pr
+  # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+  config.action_mailer.perform_cachin
