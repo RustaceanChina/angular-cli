9165f26181dcc1aa91201de06f750c936efaff54
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-08-03 06:01:47 +0000

    added copyright notices

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,4 +30,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method ac
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_m
