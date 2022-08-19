14586213f9995f3f7a9ad8f829845aeeb9213cd5
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-08-19 10:26:34 +0000

    ARM: OMAP3: hwmod data: Correct clock domains for USB modules

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,5 @@
 Config.setup do |config|
   # Name of the constant exposing loaded settings
-  config.con
+  config.const_name = 'Settings'
+
+  # Ability to remove elements of the array set in earlier loaded settings file. For examp
