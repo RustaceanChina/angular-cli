742ac8018a55e4f165f11f57149c71ba4b408456
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-08-20 09:40:29 +0000

    Updated project

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,4 +2,8 @@ Config.setup do |config|
   # Name of the constant exposing loaded settings
   config.const_name = 'Settings'
 
-  # Ability to remove elements of the array set in earlier loaded settings file. For examp
+  # Ability to remove elements of the array set in earlier loaded settings file. For example value: '--'.
+  #
+  # config.knockout_prefix = nil
+
+  # Overwrite arrays found in previously loaded
