16d65dd0190d43c6e8942ba747c869c593b04798
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-09-07 15:17:44 +0000

    Fix 4 port and add support for 8 port 'Unknown' PCI serial port cards

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -2,4 +2,7 @@
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
 #
-# Read the Guide for Upgrading
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
+
+# Enable per-form CSRF tokens. Previous versions had false.
+Rai
