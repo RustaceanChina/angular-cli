2e4fe8057931641f37f8d4dc2b7934827e027444
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-09-07 19:45:07 +0000

    Directory structure

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -7,4 +7,7 @@
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = true
 
-# Enable origin-checking CSRF mitigation. Previous 
+# Enable origin-checking CSRF mitigation. Previous versions had false.
+Rails.application.config.action_controller.forgery_protection_origin_check = true
+
+# Make Ruby 2.4 pr
