9a1ecbc62eaedcfd672776ea650a92df3330f9f5
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-11-01 12:21:04 +0000

    update binary

--- db/migrate/20170126185013_create_tweets.rb
+++ db/migrate/20170126185013_create_tweets.rb
@@ -1,1 +1,6 @@
-class CreateTweets < Active
+class CreateTweets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tweets do |t|
+      t.integer :tweet_id
+      t.text :text
+      t.string :catego
