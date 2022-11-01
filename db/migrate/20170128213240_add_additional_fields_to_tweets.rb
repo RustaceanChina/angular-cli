b0edd8a96f9e94b7ce7f6c19b481bdf7fddcfeb4
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2022-11-01 21:50:11 +0000

    Update notify.js

--- db/migrate/20170128213240_add_additional_fields_to_tweets.rb
+++ db/migrate/20170128213240_add_additional_fields_to_tweets.rb
@@ -1,1 +1,4 @@
-class AddAdditionalFieldsToTweets < ActiveRecord:
+class AddAdditionalFieldsToTweets < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tweets, :in_use, :boolean
+    add_column :tweets, :ignor
