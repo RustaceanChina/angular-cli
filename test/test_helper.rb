6f5aa88820e5940e7d5094c47e8fd1fb7f19cc13
RustaceanChina <105956351+RustaceanChina@users.noreply.github.com>
2023-03-02 08:06:09 +0000

    use spaces instead of tabs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-requi
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alpha
